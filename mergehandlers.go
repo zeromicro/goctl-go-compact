2df83b0d1fd147fa7b2ebff79e510f77bc080613
kim <xutao@xiaoheiban.cn>
2020-12-14 18:16:32 +0800

    refactor

--- mergehandlers.go
+++ mergehandlers.go
@@ -80,13 +80,13 @@ func main() {
 		panic(err)
 	}
 
+	if len(plugin.Style) == 0 {
+		plugin.Style = "gozero"
+	}
+
 	err = gogen.DoGenProject(plugin.ApiFilePath, plugin.Dir, strings.TrimSpace(plugin.Style))
 	if err != nil {
 		panic(err)
-	}
-
-	if len(plugin.Style) == 0 {
-		plugin.Style = "gozero"
 	}
 
 	var api = plugin.Api
